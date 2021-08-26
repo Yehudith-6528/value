596783925483bd5125f8377d7bf5f10fc60a7114
Yehudith-6528 <64416934+Yehudith-6528@users.noreply.github.com>
2021-08-26 14:56:06 +0300

    Delete server directory

--- server/routes/movies.route.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const moviesController= require('../controllers/movies.controller')
-
-router.get('/movies/get10Movies', moviesController.get10Movies)
-router.post('/movies/addMovie', moviesController.addMovie)
-
-module.exports = router;
